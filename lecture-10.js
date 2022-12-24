a65e9021e67980d8242cab3d9a2e528b5222cb48
mudassir619 <mudassirsiddiqui27@gmail.com>
2022-12-25 00:32:49 +0500

    function with perams

--- lecture-10.js
+++ lecture-10.js
@@ -8,23 +8,71 @@
 //     console.log(each)
 // })
 
-let stdFn = function(){
-    let arr = ['faraz','hadia','asma']
-    for(let eachStd=0;eachStd<arr.length;eachStd++){
-        if(arr[eachStd]=='hadia'){
-            return arr[eachStd]
-        }
-    }
+// let stdFn = function(){
+//     let arr = ['faraz','hadia','asma']
+//     for(let eachStd=0;eachStd<arr.length;eachStd++){
+//         if(arr[eachStd]=='hadia'){
+//             return arr[eachStd]
+//         }
+//     }
 
-    return 'nothing'
-}
+//     return 'nothing'
+// }
 
 // console(stdFn())
 
-let fnVariable = stdFn()
-console.log(fnVariable)
+// let fnVariable = stdFn()
+// console.log(fnVariable)
 
 
 // let arr = ['adasd','asda0','adsda']
 
 
+// to get minimum number among all
+let numbers = [5,7,3,2,7]
+
+let min = function (num){
+    let minNumber = num[0]
+
+    for(let each=0;each<num.length;each++){
+        if(minNumber>num[each]){
+            minNumber = num[each]
+        }
+    }
+
+    return minNumber
+}
+
+console.log(min(numbers))
+
+let add = function(num1,num2){
+    let addNumber = num1+num2
+    return addNumber
+}
+
+let addAll = function(arr){
+    let result=0
+    for(let each=0;each<arr.length;each++){
+        result = add(result,arr[each])
+    }
+
+    return result
+}
+
+let subtract = function(num1,num2){
+    let subtract = num1 - num2
+    if(subtract>=0){
+        return subtract
+    }
+    return 'invalid'
+}
+
+let multiply = function(num1,num2){
+    let multiply = num1*num2
+    if((multiply>20)&&(multiply<30)){
+        return multiply
+    }
+    return 'invalid'
+}
+
+console.log(multiply(3,2))
